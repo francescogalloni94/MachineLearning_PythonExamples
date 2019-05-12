44e415a00dc48c6540d263147b7624211a2e29d8
francescoGalloni94 <francesco.galloni94@gmail.com>
2019-05-12 18:16:57 +0200

    GridSearch

---
+++ ModelSelection/GridSearch.py
@@ -0,0 +1,87 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:,[2,3]].values
+y = dataset.iloc[:,4].values
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25,random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+from sklearn.svm import SVC
+classifier = SVC(kernel='rbf',random_state=0)
+classifier.fit(X_train,y_train)
+
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
+print('Confusion Matrix:')
+print(cm)
+
+#Applying K-FoldCrossValidation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator=classifier,X=X_train,y=y_train,cv=10)
+print('K-Fold Cross Validation accuracies:')
+print(accuracies)
+print('Mean accuracy:')
+print(accuracies.mean())
+print('Standar deviation accuracies:')
+print(accuracies.std())
+
+
+#Applying grid search to find best model and parameters
+from sklearn.model_selection import GridSearchCV
+parameters = [{'C':[1,10,100,1000],'kernel':['linear']},
+              {'C': [1, 10, 100, 1000], 'kernel': ['rbf'],'gamma':[0.5,0.1,0.01,0.001,0.0001]}]
+grid_search = GridSearchCV(estimator=classifier,param_grid=parameters,scoring='accuracy',cv=10)
+grid_search = grid_search.fit(X_train,y_train)
+best_accuracy = grid_search.best_score_
+print('best accuracy:')
+print(best_accuracy)
+best_parameters = grid_search.best_params_
+print('Best parameters:')
+print(best_parameters)
+
+
+from matplotlib.colors import ListedColormap
+plt.subplot(2,1,1)
+X_set,y_set = X_train,y_train
+X1,X2 = np.meshgrid(np.arange(start = X_set[:,0].min()-1,stop = X_set[:,0].max()+1,step = 0.01),
+                    np.arange(start=X_set[:,1].min()-1,stop=X_set[:,1].max()+1,step = 0.01))
+plt.contourf(X1,X2,classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75,cmap = ListedColormap(('red','green')))
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
+for i,j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j,0],X_set[y_set == j,1],
+                c=ListedColormap(('red','green'))(i),label = j)
+plt.title('Support Vector Machine (Training Set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+
+plt.subplot(2,1,2)
+X_set,y_set = X_test,y_test
+X1,X2 = np.meshgrid(np.arange(start = X_set[:,0].min()-1,stop = X_set[:,0].max()+1,step = 0.01),
+                    np.arange(start=X_set[:,1].min()-1,stop=X_set[:,1].max()+1,step = 0.01))
+plt.contourf(X1,X2,classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75,cmap = ListedColormap(('red','green')))
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
+for i,j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j,0],X_set[y_set == j,1],
+                c=ListedColormap(('red','green'))(i),label = j)
+plt.title('Support Vector Machine (Test Set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.tight_layout()
+plt.show()
